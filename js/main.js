f3142d8549e1e5196bbe8c4f9cbee70cf1fedd88
lilather <assurestudios@gmail.com>
2021-08-12 19:02:58 -0500

    fix some spacing errors with mobile

--- js/main.js
+++ js/main.js
@@ -1,35 +1,29 @@
 "use strict";
 
-function backToTop() {
+var backToTop = function backToTop() {
   //click back to top
-  var backToTop = document.getElementById("backToTop");
-  backToTop.addEventListener("click", function (e) {
-    e.preventDefault(), window.scroll({
+  console.log("ddd");
+  var backToTopIcon = document.getElementById("backToTop");
+  backToTopIcon.addEventListener("click", function (event) {
+    event.preventDefault(), window.scroll({
       top: 0,
       behavior: "smooth"
     });
   }); // when scroll down back to top show
 
-  window.addEventListener("scroll", function () {
-    var e = window.innerHeight / 2;
-    console.log(window.scrollY);
-    console.log(window.scrollY);
-
-    if (this.window.scrollY > e) {
-      console.log(backToTop);
-    }
-
-    window.scrollY > e ? backToTop.classList.add("showBackToTop") : backToTop.classList.remove("showBackToTop");
+  window.addEventListener("scroll", function (event) {
+    event = window.innerHeight / 2;
+    window.scrollY > event ? backToTopIcon.classList.add("showBackToTop") : backToTopIcon.classList.remove("showBackToTop");
   });
-}
+};
 
-function getModal() {
+var getModal = function getModal() {
   var registerModal = document.getElementById('registerModal');
   registerModal.addEventListener('show.bs.modal', function (event) {
     // Button that triggered the modal
     var button = event.relatedTarget; // Extract info from data-bs-* attributes
 
-    var day = button.getAttribute('data-bs-whatever'); // If necessary, you could initiate an AJAX request here
+    var day = button.getAttribute('data-bs-day'); // If necessary, you could initiate an AJAX request here
     // and then do the updating in a callback.
     //
     // Update the modal's content.
@@ -37,9 +31,8 @@ function getModal() {
     var modalTitle = registerModal.querySelector('.modal-title');
     var modalBodyInput = registerModal.querySelector('.modal-body input');
     modalTitle.textContent = 'Register for ' + day;
-    modalBodyInput.value = recipient;
   });
-}
+};
 
 backToTop();
 getModal();
